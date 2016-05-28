ae053b3c398a9881e242ba4e2d4cc9308f087958
Ivan Suazo <isuazo@netsti.com>
2016-05-28 12:36:14 -0600

    Inicio del Taller

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>PHP</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<style>
+			.color{
+				color: #7f3b3b;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="jumbotron">
+			<div class="container">
+				<h1><em class="color"><b>Taller de php</b></em></h1>
+				<p class="jb-content">Ejemplos que se desarrollaran en el taller</p>
+			</div>
+		</div>
+
+		<div class="container">
+			<h3 class="color"><em><strong>SINTAXIS</strong></em></h3>
+			<hr>
+			<ul>
+				<li>Delimitador - <a href="sintaxis/delimitador.php">ejemplo</a></li>
+				<li>Variables - <a href="sintaxis/variables.php">ejemplo</a></li>
+				<li>Comentarios - <a href="sintaxis/comentarios.php">ejemplo</a></li>
+				<li>Métodos - <a href="sintaxis/metodos.php">ejemplo</a></li>
+				<li>Super Globales - <a href="sintaxis/superglobales.php">ejemplo</a></li>
+				<li>Funciones Especiales - <a href="sintaxis/funcionesespeciales.php">ejemplo</a></li>
+			</ul>
+			<br>
+			<h3 class="color"><em><strong>PHP Programación Orientada a Objetos</strong></em></h3>
+			<hr>
+			<ul>
+				...
+			</ul>
+		</div>
+
+		<!-- jQuery -->
+		<script src="bower_components/jquery/dist/jquery.min.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	</body>
+</html>
